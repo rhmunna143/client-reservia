edeb7b27edec05239c501bd04700dcb6cc8f05f9
rhmunna143 <rhmunna19@gmail.com>
2023-11-06 12:57:16 +0600

    social login and blog page created

--- src/pages/blogs/Blogs.jsx
+++ src/pages/blogs/Blogs.jsx
@@ -1,11 +1,36 @@
 
+const BlogPage = () => {
+    // You can fetch your blog content from an API or use static data here
+    const blogPosts = [
+        {
+            id: 1,
+            title: 'The Art of Cooking',
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+            date: 'October 15, 2023',
+        },
+        {
+            id: 2,
+            title: 'Exploring Culinary Delights',
+            content: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...',
+            date: 'September 30, 2023',
+        },
+        // Add more blog posts as needed
+    ];
 
-const Blogs = () => {
     return (
-        <div>
-            Blogs
+        <div className="container mx-auto p-4">
+            <h1 className="text-3xl font-bold mb-4">Blog</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                {blogPosts.map((post) => (
+                    <div key={post.id} className="bg-white p-4 rounded shadow">
+                        <h2 className="text-xl font-semibold mb-2">{post.title}</h2>
+                        <p className="text-gray-600 mb-2">{post.date}</p>
+                        <p className="text-gray-800">{post.content}</p>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
 
-export default Blogs;
+export default BlogPage;
--- src/pages/login/ContinueSocial.jsx
+++ src/pages/login/ContinueSocial.jsx
@@ -1,10 +1,35 @@
+import { useContext, useState } from "react";
+import { AllContext } from "../../Hooks/AllContext";
+import toast from "react-hot-toast";
+import { Navigate } from "react-router-dom";
 
 
 const ContinueSocial = () => {
+    const [loggedUser, setLoggedUser] = useState(null)
+    const { googleSignIn, path } = useContext(AllContext)
+
+    const handleSignIn = () => {
+
+        googleSignIn()
+            .then(res => {
+                const user = res?.user;
+                setLoggedUser(user)
+                toast.success(user?.displayName + "! Welcome. Login success.")
+            })
+            .catch(err => {
+                toast.error(err?.message)
+                console.error(err)
+            })
+    }
+
     return (
         <div className="my-2">
             <p className="mb-4">or,</p>
-            <button className="border border-primary py-2 w-full rounded-xl hover:bg-black hover:text-white font-medium text-primary">Continue with Google</button>
+            <button onClick={handleSignIn} className="border border-primary py-2 w-full rounded-xl hover:bg-black hover:text-white font-medium text-primary">Continue with Google</button>
+
+            {
+                loggedUser && <Navigate to={path || "/"} />
+            }
         </div>
     );
 };
