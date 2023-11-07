d4b1ae6f7cbd7202d92d31b75ffbff1d457cbc57
rhmunna143 <rhmunna19@gmail.com>
2023-11-07 20:40:54 +0600

    top food section done

--- src/pages/home/topFoods/TopFoodCard.jsx
+++ src/pages/home/topFoods/TopFoodCard.jsx
@@ -1,9 +1,25 @@
+/* eslint-disable react/prop-types */
+import { Link } from "react-router-dom";
 
 
-const TopFoodCard = () => {
+const TopFoodCard = ({ food }) => {
+    const { image, name, category, price, _id } = food;
+
     return (
-        <div>
-            
+        <div className="shadow-md">
+            <div className="img">
+                <img src={image} alt="" className="h-56 w-full mx-auto" />
+            </div>
+
+            <div className="text p-4">
+                <h4 className="font-bold text-xl">{name}</h4>
+                <h5 className="text-lg font-medium">{category}</h5>
+                <p className="text-slate-500">Price: <span>$ {price}</span></p>
+
+                <Link to={`/details/${_id}`}>
+                    <button className="w-full bg-primary text-white py-2 mt-5 font-medium">See Details</button>
+                </Link>
+            </div>
         </div>
     );
 };
--- src/pages/home/topFoods/topFoods.jsx
+++ src/pages/home/topFoods/topFoods.jsx
@@ -1,26 +1,53 @@
+/* eslint-disable react/no-unescaped-entities */
 import { Link } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
 import SeparatorBlack from "../../../components/separator/SeparatorBlack";
+import axios from "axios";
+import Loader from "../../../components/Loader/Loader";
+import TopFoodCard from "./TopFoodCard";
 
 
 const TopFoods = () => {
-    return (
-        <div className="container mx-auto text-center py-20 text-lg px-4 lg:px-0">
-            <h6 className="text-primary font-playBall">Corporate Applications</h6>
-            <h2 className="text-5xl font-bold">Top Selling Foods</h2>
-            <p className="mt-4">
-                "Explore our top-selling dishes, crafted for your delight."
-            </p>
 
-            <div className="w-fit mx-auto">
-                <SeparatorBlack></SeparatorBlack>
+    const { isLoading, data: topFoods, error } = useQuery({
+        queryKey: ["topFoods"],
+        queryFn: async () => {
+            const res = await axios.get("http://localhost:8070/top-foods")
+
+            return res?.data;
+        }
+    })
+
+    if (isLoading) {
+        return <Loader />
+    }
+
+    if (error) {
+        console.log(error?.message)
+    }
+
+    return (
+        <div className="container mx-auto mt-20 pt-20 text-lg px-4 lg:px-0">
+            <div className="text-center">
+                <h6 className="text-primary font-playBall">Corporate Applications</h6>
+                <h2 className="text-5xl font-bold">Top Selling Foods</h2>
+                <p className="mt-4">
+                    "Explore our top-selling dishes, crafted for your delight."
+                </p>
+
+                <div className="w-fit mx-auto">
+                    <SeparatorBlack></SeparatorBlack>
+                </div>
             </div>
 
             {/* Cards here */}
-            <div className="mt-20">
-
+            <div className="my-20  grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                {
+                    topFoods?.map(food => <TopFoodCard key={food?._id} food={food} />)
+                }
             </div>
 
-            <div>
+            <div className="w-fit mx-auto">
                 <Link to={"/all"}>
                     <button className="bg-primary text-white font-medium px-4 py-2 hover:bg-black">See All Foods</button>
                 </Link>
