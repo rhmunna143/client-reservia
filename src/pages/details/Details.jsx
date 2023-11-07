52186cb1cb22c262a0f763e5e642570712f96d3e
rhmunna143 <rhmunna19@gmail.com>
2023-11-07 18:27:34 +0600

    order page done

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,24 +1,82 @@
-import { useQuery } from "@tanstack/react-query";
+/* eslint-disable no-unused-vars */
 import axios from "axios";
 import { useParams } from "react-router-dom";
-
+import { useContext, useEffect, useState } from "react";
+import { AllContext } from "../../Hooks/AllContext";
+import toast from "react-hot-toast";
+import Loader from "../../components/Loader/Loader";
 
 const Details = () => {
+    const [food, setFood] = useState({})
+    const [isLoading, setIsLoading] = useState(true)
+
+    const { user } = useContext(AllContext)
     const { id } = useParams()
+    const buyerId = user?.uid;
 
-    const { data: food, isLoading } = useQuery({
-        queryKey: ["food"],
-        queryFn: async () => {
-            const res = await axios.get(`http://localhost:8070/foods/${id}`)
+    const { name, image, category, userName, price, origin, description, count, quantity, uid } = food;
 
-            return res.data;
+
+    useEffect(() => {
+        axios.get(`http://localhost:8070/foods/${id}`)
+            .then(res => {
+                setFood(res?.data)
+                setIsLoading(false)
+            })
+
+    }, [id])
+
+    if (isLoading) {
+        return <Loader />
+    }
+
+    const handleOrder = (ordered, stock) => {
+        const sellerId = uid;
+        let quantity = parseInt(stock);
+        let count = ordered;
+
+
+
+        if (buyerId == sellerId) {
+            return toast.error("You can not buy your won food!!! Try another one.")
         }
-    })
 
-    const { name, image, category, userName, uid, email, price, origin, description } = food;
+        if (stock < 1) {
+            return toast.error("Out of Stock. Try again Later")
+        }
 
-    console.log(uid, email);
+        count++;
+        quantity--;
 
+        axios.post(`http://localhost:8070/order?uid=${buyerId}`, {
+            name,
+            image,
+            category,
+            userName,
+            price,
+            origin,
+            description,
+            count,
+            quantity,
+            uid,
+            buyerId
+        }, {
+            withCredentials: true
+        })
+            .then(res => {
+                if (res?.data?.insertedId) {
+                    // update count and quantity here
+
+
+                    toast.success("You Ordered Successfully! See in My Order Page.")
+                }
+            })
+            .catch(err => {
+                toast.error(err?.message)
+                console.log(err.message);
+            })
+
+    }
 
     return (
         <div className="container mx-auto my-20">
@@ -34,12 +92,14 @@ const Details = () => {
                     <p><span className="font-bold">Made by: </span> {userName}</p>
                     <p><span className="font-bold">Origin: </span> {origin}</p>
 
+                    <p><span className="text-fuchsia-500">In Stock:</span> {quantity} item/s</p>
+
                     <p className="max-w-lg italic">
                         {description}
                     </p>
 
                     <div className="w-fit mx-auto mt-5">
-                        <button className="bg-primary text-white px-4 py-2 mt-5 hover:bg-black">Order Now</button>
+                        <button onClick={() => handleOrder(count, quantity)} className="bg-primary text-white px-4 py-2 mt-5 hover:bg-black">Order Now</button>
                     </div>
                 </div>
             </div>
