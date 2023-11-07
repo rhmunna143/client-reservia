499b0de44aff961bc5f83041b14ce01488649020
rhmunna143 <rhmunna19@gmail.com>
2023-11-08 03:03:06 +0600

    search function done

--- src/pages/allfood/AllFood.jsx
+++ src/pages/allfood/AllFood.jsx
@@ -5,30 +5,46 @@ import AllFoodBanner from "./AllFoodBanner";
 import axios from "axios";
 import Loader from "../../components/Loader/Loader";
 import FoodCard from "./FoodCard";
+import { useState } from "react";
 
 
 const AllFood = () => {
+    const [foods, setFoods] = useState([])
 
-    const { error, isError, isLoading, data: foods } = useQuery({
-        queryKey: ["foods"],
+    const { error, isError, isLoading, data: initFoods } = useQuery({
+        queryKey: ["initFoods"],
         queryFn: async () => {
             const res = await axios.get("http://localhost:8070/foods")
-            
+
+            setFoods(res?.data)
             return res?.data;
         }
     })
 
-    if(isLoading) {
+    if (isLoading) {
         return <Loader />
     }
 
-    if(error){
+    if (error) {
         console.log(error?.message);
+    }
+
+    const handleSearch = (search) => {
+
+        axios.get(`http://localhost:8070/api/foods/search?search=${search}`, {
+            withCredentials: true
+        })
+            .then(res => {
+                setFoods(res?.data)
+            })
+            .catch(err => {
+                console.log(err.message);
+            })
     }
 
     return (
         <div>
-            <AllFoodBanner></AllFoodBanner>
+            <AllFoodBanner handleSearch={handleSearch}></AllFoodBanner>
 
             <div className="w-fit mx-auto text-center my-20">
                 <h6 className="text-primary text-lg  font-playBall">Corporate Application</h6>
--- src/pages/allfood/AllFoodBanner.jsx
+++ src/pages/allfood/AllFoodBanner.jsx
@@ -1,15 +1,23 @@
+import { useState } from "react";
 import "./allFood.css"
 
-const AllFoodBanner = () => {
+const AllFoodBanner = ({ handleSearch }) => {
+    const [input, setInput] = useState("")
+
+    const searchInput = (e) => {
+
+        setInput(e?.target?.value)
+    }
+
     return (
         <div className="banner-bg py-[200px]">
             <div className="w-fit mx-auto">
-                <input placeholder="Enter Search Query" className="px-4 py-2 border border-primary w-96 bg-transparent" type="text" />
+                <input onChange={searchInput} placeholder="Enter Search Query" className="px-4 py-2 border text-white border-primary w-96 bg-transparent" type="text" />
 
                 <br /> <br />
 
                 <div className="flex flex-col items-end">
-                    <button className="px-8 py-2 border border-primary bg-transparent text-white">Search</button>
+                    <button onClick={() => handleSearch(input)} className="px-8 py-2 border border-primary bg-transparent text-white">Search</button>
                 </div>
             </div>
         </div>
